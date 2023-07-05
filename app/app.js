8ad1edbd96b0d5a47eea1666bbdd7f6a15bc71ce
MechaChen <tumlivein@gmail.com>
2023-07-06 00:00:29 +0800

    build: add dockerfile

---
+++ app/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      "id":"1",
+      "title":"Book Review: The Bear & The Nightingale"
+    },
+    {
+      "id":"2",
+      "title":"Game Review: Pokemon Brillian Diamond"
+    },
+    {
+      "id":"3",
+      "title":"Show Review: Alice in Borderland"
+    }
+  ])
+})
+
